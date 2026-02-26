30768d774bc1ed9e46a7728d543fa231b153c4be
Bruno da Silva Lopes <bruno@iMac-de-Bruno-Lopes.local>
2026-02-26 13:15:50 -0300

    Creating the strucuture of components.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import Article from "./components/Article"
+import Header from "./components/Header"
+
 function App() {
-  return <h1>FIAP - Blog</h1>
+  return (
+    <>
+      <Header />
+      <Article />
+      <h1>FIAP - Blog</h1>
+    </>
+  )
 }
 
 export default App
